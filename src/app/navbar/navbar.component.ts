caddf8db0920a3d1a20a7818756379896d10166a
Clementain <clentecarmona@gmail.com>
2023-08-23 00:45:48 -0500

    Ya sirve el javascript para los navlinks

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,17 +1,15 @@
-import { Component, OnInit, Renderer2 } from '@angular/core';
+import { Component } from '@angular/core';
+
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent implements OnInit {
-  constructor(private renderer: Renderer2) { }
+export class NavbarComponent {
+  activeLink: string = '';
 
-  ngOnInit(): void {
-    const script = this.renderer.createElement('script');
-    script.type = 'text/javascript';
-    script.src = '../assets/js/main.js'; 
-    this.renderer.appendChild(document.body, script);
+  setActiveLink(link: string) {
+    this.activeLink = link;
   }
 }
---
+++ src/assets/js/main.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var navLinks = document.querySelectorAll(".nav-link");
+    
+    navLinks.forEach(function(link) {
+        link.addEventListener("click", function(event) {
+            navLinks.forEach(function(link) {
+                link.classList.remove("active");
+            });
+            event.target.classList.add("active");
+        });
+    });
+});
