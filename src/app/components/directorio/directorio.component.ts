e93bb2274029f7c54e2fb9cce2e8563633e819c6
Clementain <clentecarmona@gmail.com>
2023-09-12 17:14:27 -0700

    Se gargearon más detalles a los componentes

--- src/app/components/directorio/directorio.component.ts
+++ src/app/components/directorio/directorio.component.ts
@@ -6,5 +6,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./directorio.component.css']
 })
 export class DirectorioComponent {
+  mostrarTodos = false;
+  mostrarTiendas = false;
+  mostrarRestaurantes = false;
+  mostrarHoteles = false;
 
+  mostrarImagenes(opcion: string) {
+    this.mostrarTodos = false;
+    this.mostrarTiendas = false;
+    this.mostrarRestaurantes = false;
+    this.mostrarHoteles = false;
+
+    if (opcion === 'todos') {
+      this.mostrarTodos = true;
+    } else if (opcion === 'tiendas') {
+      this.mostrarTiendas = true;
+    } else if (opcion === 'restaurantes') {
+      this.mostrarRestaurantes = true;
+    } else if (opcion === 'hoteles') {
+      this.mostrarHoteles = true;
+    }
+  }
 }
